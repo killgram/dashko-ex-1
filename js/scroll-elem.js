e5f2110a127118a6371a5161fab526e5c341b31b
killgram <killgram94@gmail.com>
2020-10-08 00:21:16 +0700

    Добавил кнопку scroll на страницу, реализовал поведение

--- js/scroll-elem.js
+++ js/scroll-elem.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
 			$('body,html').animate({scrollTop: top}, 1000);
 	});
 });
+
+$(document).ready(function(){ 
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 800) {
+		$('.scrollup').fadeIn();
+		} else {
+		$('.scrollup').fadeOut();
+		}
+	});
+	 
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 800);
+		return false;
+	});
+});
